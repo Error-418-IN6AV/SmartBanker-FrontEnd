d1d2be85dd743afc211b7542a9c7bb45ffaf89b0
jcifuentes-2018084 <jcifuentes-2018084@kinal.edu.gt>
2023-05-29 21:53:07 -0600

    Se creo estructura del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
